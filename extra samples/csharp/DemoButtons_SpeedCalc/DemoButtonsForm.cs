1600b0d7c212c5d55fe35399ba7af1be2104be46
sigsupport <55099853+sigsupport@users.noreply.github.com>
2022-05-17 15:06:24 +0100

    Add files via upload

---
+++ extra samples/csharp/DemoButtons_SpeedCalc/DemoButtonsForm.cs
@@ -0,0 +1,59 @@
+﻿/*
+ DemoButtonsForm.cs
+
+ Controlling program for the DemoButtons program which allow user to input a signature on an STU
+ and reproduces it on a Window on the PC.  Allows signature to be saved to file on disk
+
+ Copyright (c) 2015 Wacom GmbH. All rights reserved.
+
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DemoButtons
+{
+  public partial class DemoButtonsForm : Form
+  {
+    public DemoButtonsForm()
+    {
+      InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      wgssSTU.UsbDevices usbDevices = new wgssSTU.UsbDevices();
+      if (usbDevices.Count != 0)
+      {
+        try
+        {
+          wgssSTU.IUsbDevice usbDevice = usbDevices[0]; // select a device
+
+          SignatureForm demo = new SignatureForm(usbDevice);
+          demo.ShowDialog();
+          List<wgssSTU.IPenDataTimeCountSequence> penData = demo.getPenData();          
+          if (penData != null)
+          {            
+            // process penData here!
+
+            wgssSTU.IInformation information = demo.getInformation();
+            wgssSTU.ICapability capability = demo.getCapability();
+          }
+          demo.Dispose();
+        }
+        catch (Exception ex)
+        {
+          MessageBox.Show(ex.Message);
+        }
+      }
+      else
+      {
+        MessageBox.Show("No STU devices attached");       
+      }      
+    }
+  }
+}
---
+++ extra samples/csharp/DemoButtons_SpeedCalc/SignatureForm.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace DemoButtons
+{
+  partial class SignatureForm
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (m_tablet != null))
+      {
+        m_tablet.onPenData -= new wgssSTU.ITabletEvents2_onPenDataEventHandler(onPenData);
+        m_tablet.disconnect();
+        m_tablet = null;
+      }
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }              
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.SuspendLayout();
+      // 
+      // SignatureForm
+      // 
+      this.ClientSize = new System.Drawing.Size(284, 261);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+      this.MaximizeBox = false;
+      this.MinimizeBox = false;
+      this.Name = "SignatureForm";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+      this.Text = "Signature";
+      this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form2_FormClosed);
+      this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form2_Paint);
+      this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form2_MouseClick);
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+  }
+}
