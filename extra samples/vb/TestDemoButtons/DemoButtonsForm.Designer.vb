10885484e8769277d41e0ca5eafcb7eb5348f5ce
sigsupport <55099853+sigsupport@users.noreply.github.com>
2022-05-17 15:22:25 +0100

    Add files via upload

---
+++ extra samples/vb/TestDemoButtons/DemoButtonsForm.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class DemoButtonsForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSign = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.txtDisplay = New System.Windows.Forms.TextBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnSign
+        '
+        Me.btnSign.Location = New System.Drawing.Point(330, 56)
+        Me.btnSign.Name = "btnSign"
+        Me.btnSign.Size = New System.Drawing.Size(75, 23)
+        Me.btnSign.TabIndex = 0
+        Me.btnSign.Text = "Sign"
+        Me.btnSign.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Location = New System.Drawing.Point(24, 25)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(235, 150)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(330, 110)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(75, 23)
+        Me.btnClose.TabIndex = 2
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Location = New System.Drawing.Point(12, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(261, 178)
+        Me.Panel1.TabIndex = 3
+        '
+        'txtDisplay
+        '
+        Me.txtDisplay.Location = New System.Drawing.Point(12, 210)
+        Me.txtDisplay.Multiline = True
+        Me.txtDisplay.Name = "txtDisplay"
+        Me.txtDisplay.Size = New System.Drawing.Size(424, 118)
+        Me.txtDisplay.TabIndex = 4
+        '
+        'DemoButtonsForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(450, 340)
+        Me.Controls.Add(Me.txtDisplay)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.btnSign)
+        Me.Name = "DemoButtonsForm"
+        Me.Text = "DemoButtons"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnSign As System.Windows.Forms.Button
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents btnClose As System.Windows.Forms.Button
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents txtDisplay As System.Windows.Forms.TextBox
+
+End Class
---
+++ extra samples/vb/TestDemoButtons/DemoButtonsForm.vb
@@ -0,0 +1,66 @@
+﻿'
+' DemoButtonsForm.vb
+'
+' Allow user to input a signature
+'
+' Copyright (c) 2015 Wacom GmbH. All rights reserved.
+'
+'
+Public Class DemoButtonsForm
+
+    Private Sub DemoButtonsForm_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        print("Ready")
+    End Sub
+
+    Private Sub btnSign_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSign.Click
+        Dim usbDevices = New wgssSTU.UsbDevices()
+        If (usbDevices.Count <> 0) Then
+            Try
+                print("Found " + usbDevices.Count.ToString() + " devices")
+                Dim usbDevice = usbDevices(0)
+                Dim demo = New SignatureForm(Me, usbDevice)
+                Dim res = demo.ShowDialog()
+                print("SignatureForm returned: " + res.ToString())
+                If (res = DialogResult.OK) Then
+                    DisplaySignature(demo)
+                End If
+
+            Catch ex As Exception
+                MsgBox("Exception: " + ex.Message)
+            End Try
+
+        Else
+            print("No devices found")
+        End If
+    End Sub
+
+    Private Sub btnClose_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnClose.Click
+        Me.Close()
+    End Sub
+    Public Sub print(ByVal txt As String)
+        txtDisplay.Text += txt + vbCrLf
+        txtDisplay.SelectionStart = txtDisplay.Text.Length ' scroll to end
+        txtDisplay.ScrollToCaret()
+    End Sub
+    Private Sub DisplaySignature(ByVal demo As SignatureForm)
+        Dim bitmap As Bitmap
+
+        bitmap = demo.GetSigImage()
+        ' resize the image to fit the screen
+        Dim scale = 2       ' halve or quarter the image size
+        If bitmap.Width > 400 Then
+            scale = 4
+        End If
+        PictureBox1.Size = New Size(bitmap.Width / scale, bitmap.Height / scale)
+        PictureBox1.Image = bitmap
+        'centre the image in the panel
+        Dim x, y As Integer
+        x = Panel1.Location.X + ((Panel1.Width - PictureBox1.Width) / 2)
+        y = Panel1.Location.Y + ((Panel1.Height - PictureBox1.Height) / 2)
+        Me.PictureBox1.Location = New Point(x, y)
+
+        PictureBox1.BringToFront()
+
+        'bitmap.Save("C:\\temp\\sig.png", Imaging.ImageFormat.Png) ' to save the image to disk
+    End Sub
+End Class
